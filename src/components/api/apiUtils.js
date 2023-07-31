c48d7a2f46e11b68d779a971864a1a6103812591
mukeshblackhat <mm9636389895@gmail.com>
2023-07-31 09:06:36 +0530

    minor config changes

--- src/components/api/apiUtils.js
+++ src/components/api/apiUtils.js
@@ -4,7 +4,6 @@ const clientId = process.env.NEXT_PUBLIC_CLIENTID; // Replace 'YOUR_CLIENT_ID_HE
 
 export const fetchRandomPhotos = async () => {
   try {
-    console.log(clientId)
     const response = await axios.get(`https://api.unsplash.com/photos/random?client_id=${clientId}&count=10`);
     return response.data;
   } catch (error) {

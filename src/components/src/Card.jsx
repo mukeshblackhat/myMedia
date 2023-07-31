a95bdc44d5f151b3ecb9e3688c2df3e04968e5a6
mukeshblackhat <mm9636389895@gmail.com>
2023-07-31 10:14:34 +0530

    like functionality added

--- src/components/src/Card.jsx
+++ src/components/src/Card.jsx
@@ -3,12 +3,18 @@ import styles from "../styles/card.module.css";
 import DownloadLogo from "../assets/download.svg";
 import eye from "../assets/eye.svg"
 import heartBlank from "../assets/heart-blank.svg"
+import redHeart from "../assets/red-heart-svgrepo-com.svg"
+import {useState} from "react"
 
 const Card = (data) => {
   const reqData = data.data;
-  const { user, urls, views, likes, downloads, description } = reqData;
+  const { user, urls, views, likes, downloads, description,liked_by_user } = reqData;
   const { username, profile_image } = user;
-
+  const  [like,setLike]=useState(liked_by_user);
+  console.log(like)
+  const handleClick=()=>{
+    setLike(!like)
+  }
   return (
     <>
       <div className={styles.card}>
@@ -23,8 +29,14 @@ const Card = (data) => {
           <div className={styles.statItem}>
             <img className={styles.icons} src={eye.src}/>
             {views}</div>
-          <div className={  styles.likeHover}>
+          <div className={styles.likeHover} onClick={handleClick}>
+            {like?(
+              <img className={styles.icons}  src={redHeart.src}/>
+            ):(
             <img className={styles.icons} src={heartBlank.src}/>
+             
+            )}
+            
             {likes}</div>
           <div className={styles.statItem}>
            
